62033a5b4cd9c63ee06fcd8e49354a3fbc9975d9
skanakabakwiye <s.kanakabakwiye@gmail.com>
2022-04-12 11:58:30 -0700

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,16 @@
 #include "main.h"
-
 /**
  * main - Entry point
- *
+ * 
  * Return: Always 0 (Success)
  */
 
-int main (void)
+int main(void)
+
 {
-	aputchar("c");
-	aputchar("\n");
+	_putchar('_');
+	_putchar('a');
+	_putchar('\n');
+
 	return (0);
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,10 +1,10 @@
-int aputchar(char* c);
+int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
-int _low(int c);
-int _alpha(int c);
+int _islower(int c);
+int _isalpha(int c);
 int print_sign(int n);
-int aabs(int);
+int _abs(int);
 int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
