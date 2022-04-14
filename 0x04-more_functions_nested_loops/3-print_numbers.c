b288c381216ccb149a22d1d4da0c06b036b856cb
skanakabakwiye <s.kanakabakwiye@gmail.com>
2022-04-14 13:43:16 -0700

    Task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * void - print numbers
+ *
+ * */
+
+void print_numbers(void)
+{
+	int i;
+	for(i=0; i<10; i++)
+	{
+		_putchar(i+'0');
+	}
+	_putchar("\n");
+}
