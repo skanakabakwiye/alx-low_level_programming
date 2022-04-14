87ba5724e02dcfd1a8024dd9342f1c8ec300e54f
skanakabakwiye <s.kanakabakwiye@gmail.com>
2022-04-14 13:23:09 -0700

    Task 1

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * check code
+ * Always return 0
+ */
+int _isdigit(int c)
+{
+	if(c>= '0' && c<= '9')
+	{
+	return (1);
+	}
+	else
+	{
+	retun (0);
+	}
+}
