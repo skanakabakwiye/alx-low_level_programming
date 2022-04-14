334f8ab3ce7c20311a1a4a6cdfd7fad17de060a2
skanakabakwiye <s.kanakabakwiye@gmail.com>
2022-04-14 13:11:19 -0700

    Task 0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -6,22 +6,14 @@
  *
  * Return: Always 0.
  */
-
-int main(void)
+int _isupper(int c)
 {
-    char b;
-    int c;
-    while(c<1)
-    {
-
-    while(b = 'A')
-    {
-    printf("%b: %d\n", c, _isupper(c));
-    }
-    while (b = 'a')
-    {
-    printf("%b: %d\n", c, _isupper(c));
-    }
-    }
-    return (0);
+	if(c>+'A' && c<= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }
