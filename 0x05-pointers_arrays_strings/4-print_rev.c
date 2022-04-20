a4d6e121d010ee946262d33540b272732fd79fcf
skanakabakwiye <99491003+skanakabakwiye@users.noreply.github.com>
2022-04-20 20:58:00 +0200

    Update 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,6 +1,6 @@
 #include "main.h"
 
-/** 
+/**
  * print_rev - print in reverse
  *@s: character
  */
@@ -8,14 +8,14 @@ void print_rev(char *s)
 {
 	int c, d;
 	int a = 0;
-	
+
 	while (s[a] != '\0')
 	{
 		a++;
 	}
-	
+
 	c = a;
-	
+
 	for (d = c - 1; d >= 0; d--)
 	{
 		_putchar(s[d]);
