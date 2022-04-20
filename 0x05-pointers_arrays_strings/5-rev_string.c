a3df09c9736db2efbc8f612640c5f46cb13f0aa4
skanakabakwiye <99491003+skanakabakwiye@users.noreply.github.com>
2022-04-20 21:10:55 +0200

    Update 5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -6,5 +6,22 @@
 
 void rev_string(char *s)
 {
-	int n = 7;
-	for (n = 0; s[n] != '\0'; n++)
+	int n;
+	char rev;
+	int r = 0;
+	int z = 0;
+	
+	while (s[r] != '\0')
+	{
+		r++;
+	}
+	
+	z = r - 1;
+	
+	for (n = 0; n < r/2; n++)
+	{
+		rev = s[n];
+		s[n] = s[z];
+		s[r--] = rev;
+	}
+}
