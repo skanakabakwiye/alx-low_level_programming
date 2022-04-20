6bd298d28bbaba44f5b1b6e29ff4012605f92128
skanakabakwiye <99491003+skanakabakwiye@users.noreply.github.com>
2022-04-20 21:22:13 +0200

    Update 6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,13 +1,14 @@
 #include "main.h"
 /**
- * function prints every other charactr of a string
- **/
+ * puts2 - function prints every other charactr of a string
+ *@str: variable
+ */
 
 void puts2(char *str)
 {
 	char str[20] = "Sample string";
 	int i = 0;
-	while(str != '\0')
+	while(str[i] != '\0')
 	{
 		str+=2;
 	}
