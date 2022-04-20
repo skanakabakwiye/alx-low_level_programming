5eff9a3b6a4aba10278531a1dcd13c9b3e65b118
skanakabakwiye <s.kanakabakwiye@gmail.com>
2022-04-20 10:49:12 -0700

    Task 2

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -11,8 +11,8 @@ int _strlen(char *s)
 
 int main ()
 {
-	char st[5]={'a','b','c','d','\0'};
-	_putchar("length is %d", _strlen(char st));
+	*s[5]={'a','b','c','d','\0'};
+	_putchar("length is %d", _strlen(char *s));
 	return (0);
 
 }
