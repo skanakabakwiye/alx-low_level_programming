dceb89d5c92e26f710dbcf65e9f49990024d09a9
skanakabakwiye <s.kanakabakwiye@gmail.com>
2022-04-20 10:56:46 -0700

    Task 2

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,19 +1,14 @@
 #include "main.h"
 /**
- * main - entry point
- * 
+ * int - entry point
+ * @s: character
  * return: 0
  */
 
 int _strlen(char *s)
 {
-}
-
-int main ()
-{
-	*s[5]={'a','b','c','d','\0'};
-	_putchar("length is %d", _strlen(char *s));
-	return (0);
-
+	char s[] = "Random string";
+	int i;
+	for(i=0; s[i] != '\0'; i++)
 }
 
