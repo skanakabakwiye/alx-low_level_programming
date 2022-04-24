d3415fb50dd3ad160575ca9ea75de8bf18f8364a
skanakabakwiye <99491003+skanakabakwiye@users.noreply.github.com>
2022-04-24 15:50:40 +0200

    Update 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -6,21 +6,14 @@
  * return: pointer to dest
  */
 
-char *_strcat(char *dest, char *src)
+char *_strcat(char *dest, char *src);
 {
-	int dest_length = _strlen(dest);
-	int src_length = _strlen(src);
-	int size = dest_length + src_length + 1;
-	char *s;
-	int i;
+	int s = 0;
+	int i = 0;
 
-	for (int i = 0; i < dest_length; i++)
-		s[i] = dest[i];
+	for (int i = 0; dest[i] != '\0'; i++)
 
-	for (int i = 0; i < src_length; i++)
-		s[dest_length+i] = src[i];
-
-	s[size-1] = '\0';
+	for (int s = 0; src[s] != '\0'; s++)
 
 	return (dest);
 }
