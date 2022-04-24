a91f621b941881f5432a3755b99351df19d76a5a
skanakabakwiye <s.kanakabakwiye@gmail.com>
2022-04-24 07:44:27 -0700

    Update

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/*
+ * *_strncat - concatenate two strings
+ * @dest: string
+ * @src: string
+ * @n: integer
+ * return: dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0;
+	int j = 0;
+
+	while (dest[i] != '\0')
+		i++;
+
+	while (src[j] != '\0' && j < n)
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+
+	dest[i] = '\0';
+
+	return (dest);
+}
