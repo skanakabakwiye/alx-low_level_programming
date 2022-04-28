6be52e0c3110b0df0ebb4ff2440fead4bcd0dc49
skanakabakwiye <s.kanakabakwiye@gmail.com>
2022-04-28 09:16:22 -0700

    Update

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * *_strlen_recursion - returns length
+ * @s: string to
+ * Return: return (0)
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	return (1 + _strlen_recursion(s + 1));
+}
